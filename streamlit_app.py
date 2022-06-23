5ff84a3de849c24e7f12c6c00674338a845de493
Vijay Sharma <33144977+vijay8026@users.noreply.github.com>
2022-06-23 22:07:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit
 import pandas
 import json
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('Omega 3 & Blueberry Oatmeal 🥣 ')
@@ -14,16 +18,23 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
-streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+# Section to disply Fruityvice API response.
 
-import snowflake.connector
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * From Fruit_load_list ")
@@ -36,3 +47,4 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('Which fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice)
 
+my_cur.execute("INSERT INTO Fruit_load_list values ('from streamlit')") 
